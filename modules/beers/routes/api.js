d46afe32b4d455441251a98e1f296a1d0d4cb692
dsonda <dsonda@gmail.com>
2015-10-24 12:39:49 -0200

    Inclusão via express

--- modules/beers/routes/api.js
+++ modules/beers/routes/api.js
@@ -3,10 +3,13 @@ var router = express.Router();
 
 var Controller = require('./../controller');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   //res.render('index', { title: 'Listagem de Cervejas'});
   Controller.retrieve(req, res);
+});
+
+router.post('/', function(req, res, next) {
+  Controller.create(req, res);
 });
 
 module.exports = router;
