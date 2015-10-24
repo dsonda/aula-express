20564186510ff490ac4cb24a49b68a754c32bac8
dsonda <dsonda@gmail.com>
2015-10-24 13:20:49 -0200

    Modulo de listagem de cervejas

---
+++ modules/beers/routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+var Controller = require('./../controller');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  Controller.list(req, res);
+  //res.render('list', { title: 'Listagem de Cervejas' });
+});
+
+module.exports = router;
